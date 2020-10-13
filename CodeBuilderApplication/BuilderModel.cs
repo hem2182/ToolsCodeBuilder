20ab42cfa74e190f858c3a39beb4bf038c6af42e
hem2182 <hemant_2182_fresco@yahoo.com>
2020-10-13 17:47:30 +0530

    Tools code builds application

---
+++ CodeBuilderApplication/BuilderModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBuilderApplication
+{
+    public class BuilderModelTools
+    {
+        public string ToolName { get; set; }
+
+        public List<ToolClass> ToolClasses { get; set; } = new List<ToolClass>();
+
+    }
+
+    public class ToolClass
+    {
+        public string ClassName { get; set; }
+
+        public List<ClassMethod> ClassMethods { get; set; } = new List<ClassMethod>();
+    }
+
+    public class ClassMethod
+    {
+        public string MethodName { get; set; }
+
+        public string ReturnType { get; set; }
+
+        public List<MethodParameter> MethodParameters { get; set; } = new List<MethodParameter>();
+    }
+
+    public class MethodParameter
+    {
+        public string ParameterName { get; set; }
+
+        public string ParameterType { get; set; }
+    }
+}
---
+++ CodeBuilderApplication/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBuilderApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BuilderModelTools> modelTools = new List<BuilderModelTools>();
+
+            DirectoryInfo d = new DirectoryInfo(@"C:/Code/ToolsCodeBuilder/CodeBuilderApplication/bin/DependenciesTools");
+            var dllFiles = d.GetFiles("*.dll");
+            foreach (var files in dllFiles)
+            {
+                Assembly asm = Assembly.LoadFile(files.FullName);
+                var classes = asm.GetTypes().Where(x => x.IsClass == true);
+                var c = new BuilderModelTools { ToolName = asm.GetName().ToString() };
+                modelTools.Add(c);
+                foreach (Type typeInfo in classes)
+                {
+                    MethodInfo[] classMethods = typeInfo.GetMethods();
+                    Console.WriteLine();
+                    Console.WriteLine("ToolName = " + typeInfo.Namespace);
+                    Console.WriteLine("ClassName = " + typeInfo.Name);
+                    var t = new ToolClass
+                    {
+                        ClassName = typeInfo.Name
+                    };
+                    c.ToolClasses.Add(t);
+
+                    foreach (var methodName in classMethods)
+                    {
+                        var methods = new ClassMethod()
+                        {
+                            MethodName = methodName.Name,
+                            ReturnType = methodName.ReturnType.ToString()
+                        };
+                        t.ClassMethods.Add(methods);
+                        Console.WriteLine(methodName.Name);
+                    }
+
+                }
+            }
+            WriteCode(modelTools);
+            Console.ReadKey();
+            
+        }
+
+        private static void WriteCode(List<BuilderModelTools> modelTools)
+        {
+            var jsonObj = Newtonsoft.Json.JsonConvert.SerializeObject(modelTools);
+        }
+    }
+}
